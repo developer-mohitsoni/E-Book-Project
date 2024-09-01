e938e195110510a1f9bf7c9f6c44a30563234251
Mohit Soni <developer_mohitsoni@outlook.com>
2024-09-01 10:31:35 +0530

    Add user model

--- elib/src/user/userController.ts
+++ elib/src/user/userController.ts
@@ -1,11 +1,25 @@
 import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
+import User from "./userModel";
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     // Validation
     const { name, email, password } = req.body;
 
     if (!name || !email || !password) {
         const error = createHttpError(400, "All fields are required.");
+
+        return next(error);
+    }
+    // Database call
+    const user = await User.findOne({
+        email: email,
+    });
+
+    if (user) {
+        const error = createHttpError(
+            400,
+            "User already exist with this email."
+        );
 
         return next(error);
     }
