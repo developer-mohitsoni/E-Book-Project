3312d31afc12ad43464dcef7390dc8ffa9740600
Mohit Soni <developer_mohitsoni@outlook.com>
2024-09-01 20:48:07 +0530

    add list book endpoint

--- elib/src/book/bookRouter.ts
+++ elib/src/book/bookRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createBook, updateBook } from "./bookController";
+import { createBook, listBooks, updateBook } from "./bookController";
 import uploadMiddleware from "../middlewares/multer";
 import authMiddleware from "../middlewares/authenticate";
 
@@ -9,5 +9,6 @@ const bookRouter = express.Router();
 
 bookRouter.post("/", authMiddleware, uploadMiddleware, createBook);
 bookRouter.put("/:bookId", authMiddleware, uploadMiddleware, updateBook);
+bookRouter.get('/', listBooks)
 
 export default bookRouter;
