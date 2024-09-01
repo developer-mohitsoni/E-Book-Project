8c4709110c0d9d677db06f4d2363df40eac3068b
Mohit Soni <developer_mohitsoni@outlook.com>
2024-09-01 18:29:48 +0530

    Add jwt verification

--- elib/src/book/bookRouter.ts
+++ elib/src/book/bookRouter.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { createBook } from "./bookController";
 import uploadMiddleware from "../middlewares/multer";
+import authMiddleware from "../middlewares/authenticate";
 
 const bookRouter = express.Router();
 
 // routes
 
-bookRouter.post("/",uploadMiddleware ,createBook);
+bookRouter.post("/",authMiddleware,uploadMiddleware ,createBook);
 
 export default bookRouter;
---
+++ elib/src/middlewares/authenticate.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { config } from "../config/config";
+import createHttpError from "http-errors";
+
+export interface AuthRequest extends Request {
+    userId: string;
+}
+
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.header("Authorization");
+
+    if (authHeader === null || authHeader === undefined) {
+        return next(createHttpError(401, "Authorization token is required."));
+    }
+
+    try {
+        const token = authHeader.split(" ")[1];
+        
+        const decoded = verify(token, config.jwtSecret as string);
+
+        // console.log("decoded", decoded);
+
+        const _req = req as AuthRequest;
+
+        _req.userId = decoded.sub as string;
+
+        next();
+    } catch (err) {
+        return next(createHttpError(401, "Token expired."));
+    }
+};
+
+export default authMiddleware;
