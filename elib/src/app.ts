02f217fef3968f91eb85aa0c3a64ff411c18ebd0
Mohit Soni <developer_mohitsoni@outlook.com>
2024-09-01 10:00:56 +0530

    Create User Router

--- elib/src/app.ts
+++ elib/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.get("/", (req, res, next) => {
         message: "Welcome to elib apis",
     });
 });
+
+app.use("/api/users", userRouter);
 
 // Global Error Handler
 app.use(globalErrorHandler);
