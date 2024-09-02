4ac0f11155226f8f24a34286ff5fd8eccb5c63c1
Mohit Soni <developer_mohitsoni@outlook.com>
2024-09-02 11:45:09 +0530

    add shadcn ui

--- elib-dashboard/src/App.tsx
+++ elib-dashboard/src/App.tsx
@@ -1,8 +1,11 @@
+import {Button} from "./components/ui/button";
+
 const App = ()=> {
 
   return (
     <>
       <h1>Welcome to E-Book Dashboard</h1>
+      <Button>Click Me</Button>
     </>
   )
 }
