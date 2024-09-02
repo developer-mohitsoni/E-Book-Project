fe4ab15a661f5655e0b82d0e7e3b6a7df4fe98c8
Mohit Soni <developer_mohitsoni@outlook.com>
2024-09-02 13:29:56 +0530

    add react tanstack query

--- elib-dashboard/src/pages/LoginPage.tsx
+++ elib-dashboard/src/pages/LoginPage.tsx
@@ -9,9 +9,23 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
+
+   const emailRef = useRef<HTMLInputElement>(null);
+   const passwordRef = useRef<HTMLInputElement>(null);
+
+   const handleLoginSubmit = () => {
+     // This will give me my value that we are enter on input form login box
+     const email = emailRef.current?.value;
+     const password = passwordRef.current?.value;
+
+     console.log("data", { email, password });
+
+     // Make server call.
+   };
 
   return (
     <section className="flex justify-center items-center h-screen">
@@ -26,6 +40,7 @@ const LoginPage = () => {
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
             <Input
+              ref={emailRef}
               id="email"
               type="email"
               placeholder="m@example.com"
@@ -34,18 +49,18 @@ const LoginPage = () => {
           </div>
           <div className="grid gap-2">
             <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
+            <Input ref={passwordRef} id="password" type="password" required />
           </div>
         </CardContent>
         <CardFooter>
           <div className="w-full">
-            <Button className="w-full">
+            <Button className="w-full" onClick={handleLoginSubmit}>
               Sign in
             </Button>
 
             <div className="mt-4 text-center text-sm">
               Create an account?{" "}
-              <Link to={"/register"} className="underline">
+              <Link to={"/auth/register"} className="underline">
                 Sign Up
               </Link>
             </div>
