ac8c05eb7eaa0ad00d6106756c344628a2646e24
Mohit Soni <developer_mohitsoni@outlook.com>
2024-09-09 13:12:16 +0530

    Refactor store.ts to remove commented out code

--- elib-dashboard/src/store.ts
+++ elib-dashboard/src/store.ts
@@ -6,11 +6,6 @@ export interface TokenStore {
   setToken: (data: string) => void;
 }
 
-// export interface ThemeStore {
-//   theme: "light" | "dark";
-//   toggleTheme: () => void;
-// }
-
 export const useTokenStore = create<TokenStore>()(
   devtools(
     persist(
@@ -22,21 +17,4 @@ export const useTokenStore = create<TokenStore>()(
     )
   )
 );
-
-// export const useThemeStore = create<ThemeStore>()(
-//   devtools(
-//     persist(
-//       (set, get) => ({
-//         theme: localStorage.getItem("theme") === "dark" ? "dark" : "light", // Get theme from localStorage
-//         toggleTheme: () => {
-//           const newTheme = get().theme === "light" ? "dark" : "light";
-//           localStorage.setItem("theme", newTheme); // Save to localStorage
-//           set({ theme: newTheme });
-//         },
-//       }),
-//       { name: "theme-store" }
-//     )
-//   )
-// );
-
 export default useTokenStore;
